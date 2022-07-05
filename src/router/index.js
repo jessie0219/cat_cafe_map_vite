6eb74e4974dec872343ae13f62e3d9c2fc18a0ae
jessie0219 <nini880219@gmail.com>
2022-07-05 13:45:42 +0800

    改成vite

---
+++ src/router/index.js
@@ -0,0 +1,32 @@
+// import { createRouter, createWebHashHistory } from "vue-router";
+// const routes = [
+//     {
+//         // path: "/", component: () => import("../components/HelloWorld.vue"),
+//     }
+// ]
+// export default createRouter({
+//     history: createWebHashHistory(),
+//     routes
+// });
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/HomePage.vue'
+import Map from '../views/MapPage.vue'
+const routes = [
+    {
+        path: '/',
+        name: 'home',
+        component: Home
+    },
+    {
+        path: '/map',
+        name: 'map',
+        component: Map
+    },
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+})
+
+export default router
