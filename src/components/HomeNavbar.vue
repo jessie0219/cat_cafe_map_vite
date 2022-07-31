4fea573be9556af9087c247ac1d8687db869d2e7
Angel <linf8980404@gmail.com>
2022-07-31 20:40:26 +0800

    feat: fix homepage navbar

--- src/components/HomeNavbar.vue
+++ src/components/HomeNavbar.vue
@@ -16,43 +16,59 @@
         <ul class="navbar-nav">
           <li class="nav-item">
             <router-link class="nav-link" to="/login">
-              <img src="../assets/icon/member.png" alt="" />
+              <div class="d-flex justify-content-start align-items-center">
+                <img src="../assets/icon/member.png" alt="" />
+                <p class="h2 fw-bold mx-2 mt-2">會員</p>
+              </div>
             </router-link>
           </li>
-          <!-- <li class="nav-item">
-            <router-link class="nav-link" to="/map">
-              <img src="../assets/icon/adoption1.png" alt="" />
-            </router-link>
-          </li> -->
           <li class="nav-item">
             <router-link class="nav-link" to="/allaboutcatnav">
-              <img src="../assets/icon/catInfo1.png" alt="" />
+              <div class="d-flex justify-content-start align-items-center">
+                <img src="../assets/icon/adoption1.png" alt="" />
+                <p class="h2 fw-bold mx-2 mt-2">領養</p>
+              </div>
             </router-link>
           </li>
-          <!-- <li class="nav-item">
+          <li class="nav-item">
             <router-link class="nav-link" to="/map">
-              <img src="../assets/icon/myFavorite1.png" alt="" />
+              <div class="d-flex justify-content-start align-items-center">
+                <img src="../assets/icon/catInfo1.png" alt="" />
+                <p class="h2 fw-bold mx-2 mt-2">貓的兩三事</p>
+              </div>
             </router-link>
-          </li> -->
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/map">
+              <div class="d-flex justify-content-start align-items-center">
+                <img src="../assets/icon/myFavorite1.png" alt="" />
+                <p class="h2 fw-bold mx-2 mt-2">我的最愛</p>
+              </div>
+            </router-link>
+          </li>
           <li class="nav-item">
             <router-link class="nav-link" to="/groupbuying">
-              <img src="../assets/icon/groupPurchase1.png" alt="" />
+              <div class="d-flex justify-content-start align-items-center">
+                <img src="../assets/icon/groupPurchase1.png" alt="" />
+                <p class="h2 fw-bold mx-2 mt-2">限時團購</p>
+              </div>
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/discount">
+              <div class="d-flex justify-content-start align-items-center">
+                <img src="../assets/icon/groupPurchase1.png" alt="" />
+                <p class="h2 fw-bold mx-2 mt-2">限時折扣</p>
+              </div>
             </router-link>
           </li>
           <br />
           <li class="nav-item">
-            <router-link class="nav-link" to="#">
-              <img src="../assets/icon/problemReport1.png" alt="" />
-            </router-link>
-          </li>
-          <li class="nav-item">
-            <router-link class="nav-link" to="#">
-              <img src="../assets/icon/sponsor.png" alt="" />
-            </router-link>
-          </li>
-          <li class="nav-item">
-            <router-link class="nav-link" to="#">
-              <img src="../assets/icon/setup.png" alt="" />
+            <router-link class="nav-link" to="/map">
+              <div class="d-flex justify-content-start align-items-center">
+                <img src="../assets/icon/problemReport1.png" alt="" />
+                <p class="h2 fw-bold mx-2 mt-2">問題回報</p>
+              </div>
             </router-link>
           </li>
         </ul>
@@ -76,9 +92,9 @@ nav {
 }
 .navbar-nav li {
   text-align: center;
+  margin-left: 100px;
 }
-.navbar-nav li:hover {
-  background: rgba(255, 232, 204, 0.848);
-  color: rgba(246, 146, 70, 0.664);
+.navbar-nav li p:hover {
+  color: rgba(255, 232, 204, 0.848);
 }
 </style>
